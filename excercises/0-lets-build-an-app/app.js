7dda7fd4ac21a59ee40299a44fd589e8c02d257f
Jeremy Pokorski <jeremypokorski@gmail.com>
2016-04-03 21:28:51 -0700

    continue

--- excercises/0-lets-build-an-app/app.js
+++ excercises/0-lets-build-an-app/app.js
@@ -6,11 +6,27 @@ var fetchUsers = (cb) => {
   },500);
 };
 
+// var login = React.createClass({
+//
+//   showSignIn () {
+//     // do sign in stuff
+//   }
+//
+//     render () {
+//       return (
+//         <div>
+//           <a onClick={showSignIn}>sign in</a>
+//           <h5>Hello {user.name}</h5>
+//         </div>
+//       )
+//     }
+// });
+
 var Header = React.createClass({
 
   render () {
     return (
-      <div style={headerStyle}>
+      <div>
         <h1 >Bloc Metrics</h1>
         <h3>Metrics for Bloc Jams</h3>
       </div>
@@ -48,25 +64,49 @@ var App = React.createClass({
       return <li key={user.name} onClick= {this.deleteUsers.bind(this, user)}>{user.name}</li>;
     });
     return (
-      <div style={headerStyle}>
-        <Header/>
-        <h1>Hello World</h1>
-        <ul>
-          {users}
-        </ul>
+    <div>
+      <div style={headerContainerStyle}>
+        <div style={headerStyle}>
+          <Header></Header>
+        </div>
+      </div>
+        <div style={containerStyle}>
+          <h1>Hello World</h1>
+          <ul>
+            {users}
+          </ul>
+        </div>
       </div>
     );
  }
 });
 
 var headerStyle = {
-  color: 'blue',
+  color: 'white',
   display: 'block',
-  position: 'fixed',
+  // position: 'fixed',
   top: '0',
-  width: '100%',
-  alignContent: 'center'
-
+  width: '50%',
+  margin: 'auto',
+  height: '25%',
+  alignContent: 'center',
+  textAlign: 'center'
 };
+
+var containerStyle = {
+  color: 'black',
+  paddingTop: '245px',
+  width: '100%',
+  margin: '25px auto',
+  textAlign: 'center'
+};
+
+var headerContainerStyle = {
+  width: '100%',
+  position: 'fixed',
+  backgroundColor: 'purple',
+  boxShadow: '3px 10px 5px 1px #002'
+
+}
 
 React.render(<App/>, document.body);
